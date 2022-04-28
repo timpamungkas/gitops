e278b1c1728d0ef388db131dd262448eda455892
timpamungkas-bfi <timpamungkas-bfi@users.noreply.github.com>
2022-04-28 07:54:04 +0000

    Prettified Code!

--- src/main/java/com/sample/gitops/GitopsApi.java
+++ src/main/java/com/sample/gitops/GitopsApi.java
@@ -2,7 +2,6 @@ package com.sample.gitops;
 
 import java.security.SecureRandom;
 import java.time.LocalTime;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
