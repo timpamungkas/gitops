5eaf2aaf82bac6e80208523977bc0d9ea50b0dea
Timotius Pamungkas (BFI) <timotius.pamungkas@bfi.co.id>
2022-04-28 14:52:24 +0700

    change to securerandom

--- src/main/java/com/sample/gitops/GitopsApi.java
+++ src/main/java/com/sample/gitops/GitopsApi.java
@@ -1,7 +1,7 @@
 package com.sample.gitops;
 
+import java.security.SecureRandom;
 import java.time.LocalTime;
-import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,45 +9,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitopsApi {
 
+	private SecureRandom secureRandom = new SecureRandom();
+
 	@GetMapping("/eight")
 	public String eight() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Eightfixagain" + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/five")
 	public String five() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Five " + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/four")
 	public String four() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Four " + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/nine")
 	public String nine() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Nine fix " + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/one")
 	public String one() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle One " + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/seven")
 	public String seven() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Seven " + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/six")
 	public String six() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Six minor " + LocalTime.now() + " " + random;
 	}
 
@@ -58,13 +60,13 @@ public class GitopsApi {
 
 	@GetMapping("/three")
 	public String three() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Three " + LocalTime.now() + " " + random;
 	}
 
 	@GetMapping("/two")
 	public String two() {
-		var random = ThreadLocalRandom.current().nextBoolean();
+		var random = secureRandom.nextBoolean();
 		return "Gradle Two " + LocalTime.now() + " " + random;
 	}
 }
